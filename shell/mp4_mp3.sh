9dbc6db0d0b92477ce6567f737a4db6824ce4828
Simone Colli <84104354+SimoneColli@users.noreply.github.com>
2023-02-28 10:17:54 +0100

    inizio aggiunta flag input

--- shell/mp4_mp3.sh
+++ shell/mp4_mp3.sh
@@ -1,16 +1,32 @@
 #!/bin/bash
+#flags:
+#-i input file name
+#-h help
+#-d destination folder
+
+
+while getopts i:d:h flag do
+	case "${flag}" in
+		i) inputName=${OPTARG};;
+		d) destFolder=${OPTARG};;
+		h) ;;
+
+	esac
+done
 
 extension=".mp4"
+mkdir "$destFolder"
 
-for i in *.mp4
+for i in "${inputName}".mp4
 do
 	file=${i/$extension}
 
 	ffmpeg -i "$i" -vn \
-       -acodec libmp3lame -ac 2 -ab 160k -ar 48000 \
+		-acodec libmp3lame -ac 2 -ab 160k -ar 48000 \
         "$file".mp3
-
 	echo "$file converted"
+	mv "$file".mp3 "$destFolder"
+	echo "$file moved"
 done
 
 #mv *.mp3 ./converted
