6979b449fb5861e1891646068a4c3625a34bbcaf
SimoneColli <scimmia2002@gmail.com>
2023-03-05 21:25:13 +0100

    mp4 to mp3 converter

--- shell/mp4_mp3.sh
+++ shell/mp4_mp3.sh
@@ -1,32 +1,81 @@
 #!/bin/bash
-#flags:
-#-i input file name
-#-h help
-#-d destination folder
 
+# Default values
 
-while getopts i:d:h: flag
+destFolder="."
+inputName="*"
+name=""
+
+# Read flags
+# i input file name
+# d destination folder
+# n new file name
+
+while getopts i:d:n: flag
 do
 	case "${flag}" in
 		i) inputName=${OPTARG};;
 		d) destFolder=${OPTARG};;
-		h) ;;
+		n) name=${OPTARG};;
 	esac
 done
 
+# Create destination folder
+if [ "$destFolder" != "." ]; then
+	mkdir "$destFolder"
+fi
+
+# Remove .mp4 extension from file name
+
 extension=".mp4"
-mkdir "$destFolder"
+inputName=${inputName/$extension}
 
-for i in "${inputName}".mp4
-do
-	file=${i/$extension}
+if [ "$inputName" == "*" ]; then
 
-	ffmpeg -i "$i" -vn \
-		-acodec libmp3lame -ac 2 -ab 160k -ar 48000 \
-        "$file".mp3
-	echo "$file converted"
-	mv "$file".mp3 "$destFolder"
-	echo "$file moved"
-done
+	# If no file name is specified all file .mp4 will be converted
 
-#mv *.mp3 ./converted
+	for i in *.mp4
+	do
+		fileName=${i/$extension}
+		
+		echo "$fileName"
+
+		ffmpeg -i "$i" -vn \
+			-acodec libmp3lame -ac 2 -ab 160k -ar 48000 \
+			"$fileName".mp3
+
+		echo "$fileName converted"
+
+		if [ "$destFolder" != "." ]; then
+			mv "$fileName".mp3 "$destFolder"
+			echo "$fileName moved to $destFolder"
+		fi
+	done
+	echo "Files converted"
+
+else
+	#Convert specified file
+
+	for i in "${inputName}".mp4
+	do	
+		fileName=${i/$extension}
+		
+		if [ "$name" != "" ]; then
+			fileName=$name
+		fi
+		echo "$fileName"
+
+		ffmpeg -i "$i" -vn \
+			-acodec libmp3lame -ac 2 -ab 160k -ar 48000 \
+			"$fileName".mp3
+
+		echo "$fileName converted"
+
+		if [ "$destFolder" != "." ]; then
+			mv "$fileName".mp3 "$destFolder"
+			echo "$fileName moved to $destFolder"
+		fi
+
+	done
+	echo "File converted"
+fi
